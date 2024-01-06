2332fef43aad626dbc448a9a290a7154a579037a
abhijivani3001 <abhijivani3001@gmail.com>
2024-01-06 14:51:53 +0530

    Revert "react-helmet test2"
    This reverts commit e3d455de22711df72307253665aaa7e30a4f2461.

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { HelmetProvider } from 'react-helmet-async';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  // <HelmetProvider>
-  <App />
-  // </HelmetProvider>
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
   // </React.StrictMode>
 );
 
